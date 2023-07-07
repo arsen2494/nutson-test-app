fc054443ee86dbf66b440f282bcc6ab4613bd30f
Arsen Adamyan <arsenadamyan2494@gmail.com>
2023-07-07 13:26:39 +0400

    Minor fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from "@angular/core";
+import { AuthStore } from "@store";
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  isLoggedIn$ = this._authStore.isLoggedIn$;
 
+  constructor(private _authStore: AuthStore) {
+  }
 }
